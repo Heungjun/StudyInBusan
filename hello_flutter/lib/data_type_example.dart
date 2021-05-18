ffbc8356d64ae9e12ab7ab4dd40f70c10cd8a595
Heungjun <gmdwns92@gmail.com>
2021-05-18 16:41:11 +0900

    2021-05-18, chapter 3

---
+++ hello_flutter/lib/data_type_example.dart
@@ -0,0 +1,32 @@
+void main() {
+  // 1.문자열
+  String str1 = 'flutter';
+  String str2 = 'google';
+  String plus = str1 + ' ' + str2;
+
+  int len = plus.length;
+  print(plus + ' => length : $len');
+
+  // 2.부울형
+  bool a = true;
+  bool b = false;
+  bool chk = a && b;
+  print('chk is $chk');
+
+  // 3.동적 타입(var)
+  var strLen = len; // 숫자형
+  var text = str1; // 문자열
+  var check = chk; // 부울형
+  var variable = text; // var형
+  print('$strLen , $text , $check , $variable');
+
+  // 컴파일 오류 (보너스)
+  //  variable = chk;
+  // print('$variable');
+
+  // 추가 테스트
+  String str3 = '출력 테스트';
+
+  print('성공 or 실패? $str3');
+  print('성공 or 실패? ${str3}');
+}
