8c5f5b5418144f733c6e5cc00637f09d5b4a5881
Heungjun <gmdwns92@gmail.com>
2021-05-31 23:59:02 +0900

    firebase init

--- meeting2/login_sample/lib/main.dart
+++ meeting2/login_sample/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_sample/src/app.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,14 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: App(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
