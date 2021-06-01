85881021db2e1e4d2cea45f8b60ac717ac03c3d9
Heungjun <gmdwns92@gmail.com>
2021-06-01 20:32:26 +0900

    sns login - google

--- meeting2/login_sample/lib/pages/home.dart
+++ meeting2/login_sample/lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:login_sample/pages/login.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,8 +8,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('firebase sns login'),
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (!snapshot.hasData)
+            return LoginWidget();
+          else
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('${snapshot.data?.displayName ?? 'Guest'}님 환영합니다.'),
+                ],
+              ),
+            );
+        },
       ),
     );
   }
